5ee0cfe112e548850ac6b34513bd5bc1cbb43dce
icaloyi4 <r.harisrisaldi@gmail.com>
2021-05-20 13:53:17 +0700

    Dark Mode

--- app/src/main/java/com/example/datapegawai/utils/App.kt
+++ app/src/main/java/com/example/datapegawai/utils/App.kt
@@ -1,6 +1,7 @@
 package com.example.datapegawai.utils
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -29,5 +30,6 @@ class App : Application() {
         preff = SharedData(applicationContext)
         if (preff.getData(keyIdPerusahaan).toString().isNotEmpty()) idPerusahaan = preff.getData(keyIdPerusahaan).toString()
         if (preff.getData(keyNamaPerusahaan).toString().isNotEmpty()) namaPerusahaan = preff.getData(keyNamaPerusahaan).toString()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 }
